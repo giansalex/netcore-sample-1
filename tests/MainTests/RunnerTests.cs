b8d4c1c423cbce1336236f46b076829f63b79f49
Giancarlos Salas <giansalex@gmail.com>
2018-05-02 12:24:28 -0500

    add fixture + fakeeasy

--- tests/MainTests/RunnerTests.cs
+++ tests/MainTests/RunnerTests.cs
@@ -5,11 +5,30 @@ using MainLibrary.Core;
 using System.Collections.Generic;
 using NFluent;
 using Shouldly;
+using AutoFixture;
+using FakeItEasy;
+using AutoFixture.Xunit2;
 
 namespace MainTests
 {
     public class RunnerTests
     {
+        private Fixture _fixture;
+        public RunnerTests()
+        {
+            _fixture = new Fixture();
+            _fixture.Register<ICalculator>(() => {
+                var calculator = A.Fake<ICalculator>();
+                A.CallTo(() => calculator.Operate(A<decimal>._, A<decimal>._)).ReturnsLazily(p => {
+                    var a = (decimal)p.Arguments[0];
+                    var b = (decimal)p.Arguments[1];
+                    return a + b;
+                });
+
+                return calculator;   
+            });
+        }
+        
         [Fact]
         public void TestCheckType()
         {
@@ -31,6 +50,22 @@ namespace MainTests
             Assert.Equal(13.5M, total);
             Check.That(total).IsStrictlyGreaterThan(0);
             total.ShouldBeGreaterThan(0);
+        }
+
+        [Fact]
+        public void TestSumFixture()
+        {
+            var calculator = _fixture.Create<ICalculator>();
+            var numbers = new List<Tuple<decimal, decimal>>
+            {
+                new Tuple<decimal, decimal>(4, 6),
+                new Tuple<decimal, decimal>(2, 8),
+            };
+
+            var runner = new Runner(calculator);
+            var total = runner.Total(numbers);
+
+            total.ShouldBe(20);
         }
     }
 }
